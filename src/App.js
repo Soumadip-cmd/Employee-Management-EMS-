bbb636d63b6c34cf3c9b4f9aec802e546bd55ae7
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-03-12 14:19:17 +0530

    fix error for routing and modification of UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Myleave from "./components/Myleave";
+import ApplyLeave from "./components/ApplyLeave";
+import Addsalary from "./components/Addsalary";
+import Managesalary from "./components/Managesalary";
+import AddDepartment from "./components/Department/AddDepartment";
+import ManageDepartment from "./components/Department/ManageDepartment";
+import AddStaff from "./components/Staff/AddStaff";
+import ManageStaff from "./components/Staff/ManageStaff";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route exact path="/Applyleave">
+            <ApplyLeave />
+          </Route>
+          <Route exact path="/addDepartment">
+            <AddDepartment />
+          </Route>
+          <Route exact path="/manageDepartment">
+            <ManageDepartment />
+          </Route>
+          <Route exact path="/addStaff">
+            <AddStaff />
+          </Route>
+          <Route exact path="/manageStaff">
+            <ManageStaff />
+          </Route>
+          <Route exact path="/leavehistory">
+            <Myleave />
+          </Route>
+          <Route exact path="/addsalary">
+            <Addsalary />
+          </Route>
+          <Route exact path="/managesalary">
+            <Managesalary />
+          </Route>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
