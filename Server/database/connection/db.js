9ddd641e2039706cf212dfe6572d4807683959bd
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-16 20:09:04 +0530

    bug fixes

--- Server/database/connection/db.js
+++ Server/database/connection/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const Connection = async () => {
   const { DB_USERNAME, DB_PASSWORD } = process.env;
   
-  const URL = `mongodb://localhost:27017`;
+  const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster.i2vdnrs.mongodb.net/curd_ems?retryWrites=true&w=majority&appName=Cluster`;
 
   try {
     await mongoose.connect(URL, {
