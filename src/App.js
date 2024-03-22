9de0c812b602a41701713c5ef49f559c575fa1af
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-22 18:18:35 +0530

    staff fix except ui

--- src/App.js
+++ src/App.js
@@ -62,9 +62,9 @@ function App() {
           <Route exact path="/error">
             <Error />
           </Route>
-          {/* <Route exact path="/staffList">
+          <Route exact path="/staffList">
             <StaffList />
-          </Route> */}
+          </Route>
           <Route exact path="/Applyleave">
             <ApplyLeave />
           </Route>
@@ -77,10 +77,10 @@ function App() {
           <Route exact path="/addStaff">
             <AddStaff />
           </Route>
-          <Route exact path="/manageStaff">
+          <Route exact path="/manageStaff/:id">
             <ManageStaff />
           </Route>
-          <Route exact path="/editStaff">
+          <Route exact path="/editStaff/:id">
             <EditStaff />
           </Route>
           <Route exact path="/leavehistory">
