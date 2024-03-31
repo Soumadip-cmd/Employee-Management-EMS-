27c457cfe7a266b0a8acfccb702083b74406ab95
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-31 22:05:03 +0530

    login and signup page added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,98 +1,99 @@
-import React, { useState } from "react";
-import styles from "./Signup.module.css";
-import InputControl from "../inputControl/inputControl";
-import { Link, useHistory } from 'react-router-dom';
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from "../firebase";
+import { useState } from "react";
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
+import styles from "./styles.module.css";
 
-export default function Signup() {
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    pass: "",
-  });
+const Signup = () => {
+	const [data, setData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+	});
+	const [error, setError] = useState("");
+	const navigate = useHistory();
 
-  // Error message
-  const navigate = useHistory();
-  const [errorMsg, setErrorMsg] = useState("");
-  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+	const handleChange = ({ currentTarget: input }) => {
+		setData({ ...data, [input.name]: input.value });
+	};
 
-//------------------Sign  up error message handling----------------------
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const url = "http://localhost:8080/api/users";
+			const { data: res } = await axios.post(url, data);
+			navigate.push("/login");
+			console.log(res.message);
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.status >= 400 &&
+				error.response.status <= 500
+			) {
+				setError(error.response.data.message);
+			}
+		}
+	};
 
+	return (
+		<div className={styles.signup_container}>
+			<div className={styles.signup_form_container}>
+				<div className={styles.left}>
+					<h1>Welcome Back</h1>
+					<Link to="/login">
+						<button type="button" className={styles.white_btn}>
+							Sing in
+						</button>
+					</Link>
+				</div>
+				<div className={styles.right}>
+					<form className={styles.form_container} onSubmit={handleSubmit}>
+						<h1>Create Account</h1>
+						<input
+							type="text"
+							placeholder="First Name"
+							name="firstName"
+							onChange={handleChange}
+							value={data.firstName}
+							required
+							className={styles.input}
+						/>
+						<input
+							type="text"
+							placeholder="Last Name"
+							name="lastName"
+							onChange={handleChange}
+							value={data.lastName}
+							required
+							className={styles.input}
+						/>
+						<input
+							type="email"
+							placeholder="Email"
+							name="email"
+							onChange={handleChange}
+							value={data.email}
+							required
+							className={styles.input}
+						/>
+						<input
+							type="password"
+							placeholder="Password"
+							name="password"
+							onChange={handleChange}
+							value={data.password}
+							required
+							className={styles.input}
+						/>
+						{error && <div className={styles.error_msg}>{error}</div>}
+						<button type="submit" className={styles.green_btn}>
+							Sing Up
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-//---------------Handel Submission Function------------------------------
-  const handleSubmission = async () => {
-    if (!values.name || !values.email || !values.pass) {
-      setErrorMsg("Fill all fields");
-      return;
-    }
-
-    setSubmitButtonDisabled(true);
-
-    // If all fields are valid, then empty the error message
-    setErrorMsg("");
-
-    try {
-      const res = await createUserWithEmailAndPassword(auth, values.email, values.pass);
-      const user = res.user;
-
-      await updateProfile(user, {
-        displayName: values.name,
-      });
-
-      setSubmitButtonDisabled(false);
-      navigate.push("/"); //redirect to original page
-    } catch (err) {
-      setSubmitButtonDisabled(false);
-      setErrorMsg(err.message);
-      console.error("Error-", err.message);
-    }
-  };
-//-----------------All Functions Ends Here----------------------------------
-
-//-----------------React JSX HTML Start--------------------------------------------
-  return (
-    <div className={styles.container}>
-      <div className={styles.innerBox}>
-      {/* -------------------Signup Heading------------------------ */}
-        <h1 className={styles.heading}>Signup</h1>
-        <InputControl
-          label="Name"
-          placeholder="Enter Your Name"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, name: event.target.value }))
-          }
-        />
-{/* ---------------------Passing the data to inputControl using props-----------------------------------  */}
-        <InputControl
-          label="Email"
-          placeholder="Enter Your Email Address"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, email: event.target.value }))
-          }
-        />
-
-        <InputControl
-          label="Password"
-          placeholder="Enter Your Password"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, pass: event.target.value }))
-          }
-        />
-{/* --------------Signup Button Handeling------------------------- */}
-        <div className={styles.footer}>
-          <b className={styles.error}>{errorMsg}</b>
-          <button onClick={handleSubmission} disabled={submitButtonDisabled}>
-            Signup
-          </button>
-          <p>
-            Already have an account?{" "}
-            <span>
-              <Link to="/login">Login</Link>
-            </span>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
+export default Signup;
