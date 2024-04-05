c06da97e449877b0f1eb2c7a12425ab4d36625a9
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-04-05 09:20:29 +0530

    user ui added

--- src/components/SignUpUser/User.js
+++ src/components/SignUpUser/User.js
@@ -27,7 +27,31 @@ export default function User() {
       {userData ? (
         <p>Welcome, {userData.user_name}</p>
       ) : (
-        <p>Loading...</p>
+        <div className="alert alert-danger d-flex align-items-center" role="alert">
+          No Staff Member Found in our Company.
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" ml-10>
+            See Reason
+          </button>
+
+          {/* Modal */}
+          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">Reason</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                  Please use your email ID provided during registration to access the your account. If you are not a staff member, this feature is not intended for you.
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
