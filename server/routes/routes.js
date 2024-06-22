383277ae36ce8ec80ea55b8ea84f1322cb8a4bf1
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-06-22 19:43:38 +0530

    admin full login done

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,13 @@ const router=express.Router()
 const UserRegistration=require('../controllers/UserRegistration')
 const checkEmail=require('.././controllers/checkEmail')
 const checkPassword=require('.././controllers/checkPassword')
+const checkAdmin=require("../controllers/checkAdmin")
 // Registration Controller
 router.post('/register',UserRegistration)
 //Email
 router.post('/email',checkEmail)
 //Check Password
 router.post('/password',checkPassword)
+//check admin
+router.post('/admin',checkAdmin)
 module.exports=router
