d53f80fee7f844ba61b84fed9cf90021ff920a93
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-06-26 20:27:33 +0530

    add staff  api added

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,7 +4,7 @@ const UserRegistration=require('../controllers/UserRegistration')
 const checkEmail=require('.././controllers/checkEmail')
 const checkPassword=require('.././controllers/checkPassword')
 const checkAdmin=require("../controllers/checkAdmin");
-
+const addStaff=require('../controllers/AddStaff/AddStaff')
 // Registration Controller
 router.post('/register',UserRegistration)
 //Email
@@ -13,6 +13,7 @@ router.post('/email',checkEmail)
 router.post('/password',checkPassword)
 //check admin
 router.post('/admin',checkAdmin)
-
+//add staff
+router.post('/addstaff',addStaff)
 
 module.exports=router;
