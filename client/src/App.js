9f02a5f60408485981d41cd7dc6287ff2a0bf51e
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-03 11:30:29 +0530

    1 update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  
+} from "react-router-dom";
+import Test from './components/Test';
 
 function App() {
+  const router = createBrowserRouter([
+    
+    {
+      path: "/",
+      element: <><Test/></>,
+    },
+    {
+      path: "/test",
+      element: <><Test/></>,
+    },
+  ]);
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ client/src/components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      This Is Test .js testing man!!
+    </div>
+  )
+}
+
+export default Test
