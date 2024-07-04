cf8d199c5f1d3a7b1c5750fe2887b1c9d67b5277
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-04 23:12:38 +0530

    updated

--- client/src/components/Staff/ManageStaff.js
+++ client/src/components/Staff/ManageStaff.js
@@ -95,7 +95,7 @@ export default function ManageStaff() {
               <tr>
                 <td>1</td>
                 <td>Soumadip santra</td>
-                <td className="d-flex justify-content-center align-items-center h-100"><img className =" rounded-pill " src="https://placeholder.co/32x32" alt="img-Profile" /></td>
+                <td className=""><img className =" rounded-2 " src="https://placehold.co/64x64" alt="img-Profile" /></td>
                 <td>Web Developement</td>
                 <td>Male</td>
                 <td>8965321459</td>
